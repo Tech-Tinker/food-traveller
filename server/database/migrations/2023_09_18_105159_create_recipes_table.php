c8c0a7fddcffffdfb8886afec397dc1cea7954a8
Rous Medina <36710468+rousmedina@users.noreply.github.com>
2023-09-19 12:07:35 +0200

    migrate

---
+++ server/database/migrations/2023_09_18_105159_create_recipes_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('recipes', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('image');
+            $table->string('description');
+            $table->string('author');
+            $table->string('time');
+            $table->string('difficulty');
+            $table->string('ingredients');
+            $table->string('preparation');
+            $table->timestamps();
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->enum('privacy', ['public', 'private'])->default('public');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('recipes', function (Blueprint $table) {
+            $table->dropColumn(['user_id', 'privacy']);
+        });
+    }
+};
