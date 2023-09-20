3cb12439d937e8e3056897b738a09b35a9389f87
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-09-20 10:53:37 +0200

    home_header_component

---
+++ client/src/pages/home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Home.css';
+import Header from '../../components/header/Header';
+
+
+const Home = () => {
+    return (
+        <main>
+            <Header />
+
+        </main>
+    )
+};
+
+export default Home;
