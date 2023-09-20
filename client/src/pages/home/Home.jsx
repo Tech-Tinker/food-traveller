01ded7a8bed04ecc2ee5755043fdd2c1d8a537af
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-09-20 15:34:32 +0200

    nav-footer_assets

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import './Home.css';
 import Header from '../../components/header/Header';
+import Nav from '../../components/nav/Nav';
 
 
 const Home = () => {
     return (
+        <>
         <main>
             <Header />
 
+            
         </main>
+        <Nav />
+        </>
     )
 };
 
