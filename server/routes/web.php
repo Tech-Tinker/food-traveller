bb4232ca3a9129dc3adb51a4d6dcaa3704f08dfb
LenaHalas <milehalasj@gmail.com>
2023-09-26 12:35:26 +0200

    pull dev

--- server/routes/web.php
+++ server/routes/web.php
@@ -22,3 +22,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
