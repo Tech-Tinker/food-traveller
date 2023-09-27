7bfb5c5bfbf069e3e94a95d6c4375d4e7d75cfcd
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-09-27 18:51:58 +0200

    header_displayNone_create-recipe

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,22 +1,38 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './Header.css';
 import Logo from '../../assets/Logo.svg';
-import Login from '../../assets/icon-login.svg';
+import Avatar from '../../assets/Avatar.svg';
 
 const Header = () => {
-   
-    return (
-        <div className='d-flex justify-content-between align-items-center header-margin'>
-            <Link to={'/'}>
-                <img src={Logo} alt="" />
-            </Link>
- {/* <Link to={'/editar-perfil'}>Editar Perfil</Link> */}
-            <Link to={'/login'}>
-                <img src={Login} alt="" />
-            </Link>
-        </div>
-    );
-}
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+  const isRegisterPage = location.pathname === '/register';
+  // const isPerfilPage = location.pathname === '/perfil';
+  const isCreateRecipe = location.pathname === '/create-recipe';
+  // const isEditPage = location.pathname === '/edit';
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <div className='d-flex justify-content-between align-items-center header-margin'>
+      <Link to={'/'}>
+        <img className='logo' src={Logo} alt='Logo Foodtraveller' />
+      </Link>
+
+      {!isLoginPage && !isRegisterPage && !isLoggedIn && !isCreateRecipe && (
+        <Link to={'/login'}>
+          <img src={Avatar} alt='Icono de Login' />
+        </Link>
+      )}
+      
+    </div>
+  );
+};
 
 export default Header;
+
