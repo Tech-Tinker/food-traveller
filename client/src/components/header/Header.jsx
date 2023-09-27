325e2d5f599cc4dcc8706f9257f8aee1f1370c6e
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-09-27 18:35:02 +0200

    header

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Header.css'
+import React, { useState } from 'react';
+import { Link, useLocation } from 'react-router-dom';
+import './Header.css';
 import Logo from '../../assets/Logo.svg';
-import Menu from '../../assets/Menu.svg';
+import Avatar from '../../assets/Avatar.svg';
 
 const Header = () => {
+  const location = useLocation();
+  const isLoginPage = location.pathname === '/login';
+  const isRegisterPage = location.pathname === '/register';
+  // const isPerfilPage = location.pathname === '/perfil';
+  // const isCreatePage = location.pathname === '/create';
+  // const isEditPage = location.pathname === '/edit';
+
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  const handleLogin = () => {
+    setIsLoggedIn(false);
+  };
+
   return (
     <div className='d-flex justify-content-between align-items-center header-margin'>
       <Link to={'/'}>
-        <img src={Logo} alt="" />
+        <img className='logo' src={Logo} alt='Logo Foodtraveller' />
       </Link>
 
-      {/* <Link to={'/perfil'}>Perfil</Link> */}
-
-      <Link to={'/login'}>
-        <img src={Menu} alt="" />
-      </Link>
-
+      {!isLoginPage && !isRegisterPage && !isLoggedIn && (
+        <Link to={'/login'}>
+          <img src={Avatar} alt='Icono de Login' />
+        </Link>
+      )}
+      
     </div>
   );
-}
+};
 
 export default Header;
