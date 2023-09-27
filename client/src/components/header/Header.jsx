56bbb34722220ee6f92c73f2402a999fefef21c5
Rous Medina <36710468+rousmedina@users.noreply.github.com>
2023-09-27 16:37:37 +0200

    codigos

--- client/src/components/header/Header.jsx
+++ client/src/components/header/Header.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
-import Logo from '../../assets/Logo.svg';
+// import Logo from '../../assets/';
 import Menu from '../../assets/Menu.svg';
 
 const Header = () => {
   return (
     <div className='d-flex justify-content-between align-items-center header-margin'>
       <Link to={'/'}>
-        <img src={Logo} alt="" />
+        {/* <img src={Logo} alt="" /> */}
       </Link>
 
-      {/* <Link to={'/perfil'}>Perfil</Link> */}
+      {/* {<Link to={'/perfil'}>Perfil</Link>} */}
 
       <Link to={'/login'}>
         <img src={Menu} alt="" />
