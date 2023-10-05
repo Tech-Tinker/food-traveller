702b6f71225c975a6e9e7d506dbc17c2a71ff0c8
leandrita <lea.bujh@gmail.com>
2023-10-05 20:48:51 +0200

    add seeders

--- server/database/seeders/UserProfilesTableSeeder.php
+++ server/database/seeders/UserProfilesTableSeeder.php
@@ -18,7 +18,7 @@ class UserProfilesTableSeeder extends Seeder
                 ['user_id' => $user1->id],
                 [
                     'user_name' => 'Julia',
-                    'profile_image' => 'profile_images/7aqnKyIbYyziVSLCnboLVseLkRUfCDCVWjWAn8P8.jpg',
+                    'profile_image' => 'profile_images/1YzVviLyvCGxuq940Xy1zCl9Zp7dDCwpH3P9MlxO.jpg',
                     'description' => 'Me gustan las especias',
                     'birthdate' => '1990-01-01',
                     'country' => 'Cuba',
@@ -34,7 +34,7 @@ class UserProfilesTableSeeder extends Seeder
                 ['user_id' => $user2->id],
                 [
                     'user_name' => 'Bruno',
-                    'profile_image' => 'profile_images/2spbJMXIMF7F69TRV4oOR1cgIxiGvXnI6rEAFoSW.jpg',
+                    'profile_image' => 'profile_images/BcVWiOTH5wWLbmlS2joyQIBp6xhAwf1C0lPC86Ua.jpg',
                     'description' => 'Amo las pastas',
                     'birthdate' => '1995-05-05',
                     'country' => 'Italia',
@@ -50,7 +50,7 @@ class UserProfilesTableSeeder extends Seeder
                 ['user_id' => $user3->id],
                 [
                     'user_name' => 'Lucia',
-                    'profile_image' => 'profile_images/MBMOHstj1NKeqGY7VAu6B3L9htpn6ZJMHkM38cHK.jpg',
+                    'profile_image' => 'profile_images/dmEnwmcksCLwF6M6jLNGgfoXJlasiGvq1cEUnRVx.jpg',
                     'description' => 'La comida me hace muy felíz!',
                     'birthdate' => '1988-12-10',
                     'country' => 'Alemania',
