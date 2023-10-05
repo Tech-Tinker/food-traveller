1cb24efda7f745483308b5e183b497f9ad32bb72
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-10-05 08:01:17 +0200

    .

--- client/src/pages/result/Result.jsx
+++ client/src/pages/result/Result.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 import Header from '../../components/header/Header';
-import Nav from '../../components/nav/Nav';
 import Search from '../../components/search/Search';
 
 const Result = () => {
