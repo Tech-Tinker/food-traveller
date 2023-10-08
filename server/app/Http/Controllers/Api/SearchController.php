499c8ed307f722ce24aceff63330091e49a5ca5f
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-10-08 19:40:11 +0200

    Api_search_Call_fixed

--- server/app/Http/Controllers/Api/SearchController.php
+++ server/app/Http/Controllers/Api/SearchController.php
@@ -3,19 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Recipe;
 
 class SearchController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth')->only('search');
-    }
-
     public function search(Request $request)
     {
         $search = $request->input('query'); 
         if (!empty($search)) {
-            $result = foodtravel::where('id', 'LIKE', "%$search%")
+            $result = Recipe::where('id', 'LIKE', "%$search%")
                             ->orWhere('title', 'LIKE', "%$search%")
                             ->orWhere('description', 'LIKE', "%$search%")
                             ->orWhere('time', 'LIKE', "%$search%")
@@ -23,12 +19,10 @@ class SearchController extends Controller
                             ->orWhere('difficulty', 'LIKE', "%$search%")
                             ->orWhere('ingredients', 'LIKE', "%$search%")
                             ->orWhere('preparation', 'LIKE', "%$search%")
-                            ->orWhere('location', 'LIKE', "%$search%")
                             ->paginate(5); 
         } else {
             $result = collect(); 
         }
-        return view('recipes.search', compact('result', 'search'));
+        return view('recipes.show', compact('result', 'search'));
     }
 }
-
