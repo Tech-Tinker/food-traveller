315cce3088748ee0595d8e63e5db0a18cbc6c994
Rafaela <125391542+todaunabossa@users.noreply.github.com>
2023-10-08 17:11:10 +0200

    fixed_function_db

--- client/src/components/search/Search.jsx
+++ client/src/components/search/Search.jsx
@@ -10,9 +10,9 @@ const Search = ({ onSearch }) => {
 
   const handleSearch = async () => {
     try {
-      const response = await axios.get(`/api/recipes?query=${query}`);
+      const response = await axios.get(`/api/search?query=${query}`);
       if (response.data.length === 0) {
-        setErrorMessage('No se encontraron recetas con las características solicitadas');
+        setErrorMessage('No se encontraron cohicidencias');
       } else {
         setErrorMessage('');
       }
@@ -42,42 +42,3 @@ const Search = ({ onSearch }) => {
 export default Search;
 
 
-
-// import React, { useState } from 'react';
-// import axios from 'axios';
-
-// const Search = ({ onSearch }) => {
-//   const [query, setQuery] = useState('');
-//   const [results, setResults] = useState([]);
-
-//   const handleSearch = async () => {
-//     try {
-//       const response = await axios.get(`/api/search?query=${query}`);
-//       setResults(response.data);
-//       onSearch(response.data); // Llama a la función de callback para pasar los resultados al componente padre (Result en este caso)
-//     } catch (error) {
-//       console.error('Error al buscar:', error);
-//     }
-//   };
-
-//   return (
-//     <div className="searchBar">
-//       <input
-//         type="text"
-//         placeholder="Buscar recetas..."
-//         value={query}
-//         onChange={(e) => setQuery(e.target.value)}
-//       />
-//       <button onClick={handleSearch}>Buscar</button>
-
-//       {/* Muestra los resultados de la búsqueda */}
-//       <ul>
-//         {results.map((result) => (
-//           <li key={result.id}>{result.title}</li>
-//         ))}
-//       </ul>
-//     </div>
-//   );
-// };
-
-// export default Search;
